74e35abc1027f328b61aefd0f92bff274ec75e46
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-08 04:05:55 -0400

    idk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,8 +23,8 @@ type ModalProps = {
 
 const Modal = ({ setShow }: ModalProps) => {
   return (
-    <div class="fixed top-0  left-0  h-screen w-full  bg-black bg-opacity-50 modal z-50">
-      <div class="bg-green-600 text-bg w-1/2 h-screen">
+    <div id="modal" class="fixed top-0  left-0  h-screen w-full  bg-black bg-opacity-50 modal z-50">
+      <div id="modal" class="bg-green-600 z-50 text-bg w-1/2 h-screen">
         <div class="flex items-center justify-between p-3">
           <A href="#hero" class="text-2xl pasifico font-bold">Lucas Sproule</A>
           <button onClick={() => setShow(false)}>Close</button>
