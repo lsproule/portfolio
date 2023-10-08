3aabcaa27f11fee7843a2c6dc2a4026a2b4755c0
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-08 03:55:01 -0400

    idk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ type ModalProps = {
 
 const Modal = ({ setShow }: ModalProps) => {
   return (
-    <div class="fixed top-0 modal left-0  h-screen w-full  bg-black bg-opacity-50  z-50">
+    <div class="fixed top-0  left-0  h-screen w-full  bg-black bg-opacity-50 modal z-50">
       <div class="bg-green-600 text-bg w-1/2 h-screen">
         <div class="flex items-center justify-between p-3">
           <A href="#hero" class="text-2xl pasifico font-bold">Lucas Sproule</A>
