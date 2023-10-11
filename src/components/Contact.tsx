ce3e05bcdd2c61f98fc73defb15f891a21af2e6b
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-11 03:58:15 -0400

    idk

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -16,7 +16,7 @@ export const Contact = () => {
         <div class="text-sm text-muted ml-2">I'm currently looking for work, if you're interested in hiring me, please reach out!</div>
         <input type="text" class="border bg-primary text-green-500 border-green-500 rounded-xl p-2 px-3 ml-2" placeholder="email" />
         <input type="text" class="border bg-primary text-green-500 border-green-500 rounded-xl p-2 px-3 ml-2" placeholder="name" />
-        <input type="text" class="border inline-block align-text-top bg-primary  text-green-500  border-green-500 rounded-xl  pt-2 pb-20 px-2  ml-2" placeholder="message" />
+        <textarea  class="border inline-block align-text-top bg-primary  text-green-500  border-green-500 rounded-xl  pt-2 pb-20 px-2  ml-2" placeholder="message" />
         <button class="border border-green-500 rounded-xl p-2 px-3 ml-2">Send</button>
 
 
