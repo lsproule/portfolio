de6dfb8e779effb63e7b59e148b536e6fb62d595
lucas.sproule <lucas.sproule.42@gmail.com>
2023-11-22 09:47:15 -0500

    almost back to full functionality

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,14 +4,14 @@ import { TbUser } from 'solid-icons/tb'
 
 export const About = () => {
   return (
-    <section id="about" class="lg:w-2/3 p-4 mx-auto    md:mb-10">
-      <Container class="h-auto  mx-auto bg-neutral-800 p-3  ">
-        <div class="text-white    flex mb-8  gap-4 text-2xl font-bold"><TbUser class="text-3xl text-green-500"/>About Me</div>
-        <div class="grid mx-auto  items-center justify-items-center md:grid-cols-4  gap-4 ">
-          <div class="w-2/3 h-full md:w-full md:col-span-1 lg:h-[40rem]">
-            <img src="/profile.jpeg" class=" md:max-h-full object-contain rounded-lg" />
+    <section id="about" class="lg:w-2/3  mx-auto bg-neutral-800   md:mb-10">
+      <Container class="h-auto  mx-auto  p-3  ">
+        <div class="text-white  px-4   flex mb-8  gap-4 text-2xl font-bold"><TbUser class="text-3xl text-green-500"/>About Me</div>
+        <div class="grid p-4 mx-auto  items-center justify-items-center md:grid-cols-4  gap-4 ">
+          <div class="w-2/3  h-full flex justify-center md:w-full md:col-span-1 lg:h-[40rem]">
+            <img src="/profile.jpeg" class=" my-auto  md:max-h-full object-contain rounded-lg" />
           </div>
-          <div class="mx-auto mb-4 w-5/6 text-base md:text-lg md:col-span-3   md:text-left">
+          <div class=" ml-4 mb-4 w-5/6 text-base md:text-lg md:col-span-3   md:text-left">
 
             I'm a devoted DevOps/SRE engineer with a love for containerization, coupled with a profound fascination for a multitude of programming languages. My insatiable curiosity is my driving force, continually propelling me towards mastering new technologies and optimizing system performance.
             I'm a tech enthusiast with a passion for travel and a knack for problem-solving
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,19 +6,21 @@ export const Contact = () => {
   return (
     <section id="contact" class="mb-8 h-full p-3 ">
       <Container class="  ">
-        <div class="flex mb-12 justify-center">
-          <div class="flex rounded-lg  w-full text-black md:w-1/2 lg:w-1/3 bg-neutral-800 p-12 flex-col mt-12 gap-4 items-center  justify-center text-xl font-bold  ">
+        <div class="flex   justify-center">
+          <div class="flex p-4 mb-8 rounded-lg  w-full text-black md:w-1/2 lg:w-1/3 bg-neutral-800  flex-col mt-12 gap-4 items-center  justify-center text-xl font-bold  ">
             <div class="flex items-center text-white mt-4 justify-center text-2xl font-bold mb-3 ">
               <FaSolidSuitcase class="mr-2 text-3xl text-green-500" /> Contact
             </div>
             <div class="text-sm text-white text-balance text-center text-muted ml-2">
               I'm currently looking for work, if you're interested in hiring me, please reach out!
             </div>
-            <Input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" variant={'default'}  placeholder="test" />
-            <input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" type="text" placeholder="email" />
-            <input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" type="text" placeholder="name" />
-            <textarea class="border w-full h-48  align-text-top border-green-500 rounded-xl  pt-2  px-3  ml-2" placeholder="message" />
-            <button class="border text-green-500 border-green-500 rounded-xl p-2 px-3 ml-2">Send</button>
+            <form action="mailto:lucas.sproule.42@gmail.com" class="flex w-full flex-col gap-4" method="post" enctype="text/plain">
+              <Input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" variant={'default'} placeholder="test" />
+              <input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" type="text" placeholder="email" />
+              <input class="border w-full bg-white border-green-500 rounded-xl p-2 px-3 ml-2" type="text" placeholder="name" />
+              <textarea class="border w-full h-48  align-text-top border-green-500 rounded-xl  pt-2  px-3  ml-2" placeholder="message" />
+              <button class="border bg-green-500 text-neutral-800 border-green-500 rounded-xl p-2 px-3 ml-2">Send</button>
+            </form>
           </div>
         </div>
       </Container>
