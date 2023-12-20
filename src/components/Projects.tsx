d7bbf9cae316d17ce818589ae5a2edb504acf199
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-20 00:43:47 -0500

    war crimes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,9 @@
 import { Container } from "./Container";
-import { For } from "solid-js"
+import { createSignal, For } from "solid-js"
 import { FaSolidSuitcase } from 'solid-icons/fa'
 import { createSlider, SliderButton, SliderProvider } from "solid-slider"
+import { A } from "solid-start";
+import { Portal } from "solid-js/web";
 
 type Project = {
   title: string,
@@ -9,36 +11,54 @@ type Project = {
   image: string,
   link?: string
   class?: string
+  info: string
 }
+const [fullscreenProject, setFullscreenProject] = createSignal<string | null>(null);
+
+const toggleFullscreen = (projectTitle: string) => {
+    setFullscreenProject(fullscreenProject() === projectTitle ? null : projectTitle);
+};
+
+const isProjectFullscreen = (projectTitle: string) => fullscreenProject() === projectTitle;
 
 const projects = [
   {
     title: "Frate - Cargo for c++",
     description: "I am working on an open source package manager for c++, that will allow you to easily install and manage c++ libraries.",
-    image: "/frate.png"
-
+    image: "/frate.png",
+    info: "/frate"
   },
   {
     title: "Embedding Neovim into the browser",
     description: "I wanted to practice with file descriptors and websockets, so I built this project to pass stdio through a socket. I built it with javascript and python. I isolated my neovim with docker to build the user interface",
-    image: "/nvim.png"
+    image: "/nvim.png",
+    info: "/nvim"
   },
   {
     title: "Open Source Contributor to astronvim",
     description: "I am a contributor to the astronvim project, which is a neovim distribution that is built with lua. I have contributed to the project by adding a new plugin, and fixing bugs.",
-    image: "/astro.png"
+    image: "/astro.png",
+    info: "/astro"
   },
 ]
-const Project = ({ title, description, image, ...props }: Project) => {
+const Project = ({ title, description, image, info, ...props }: Project) => {
   return (
-    <div  class={`slide flex flex-col justify-around  p-2 pt-5 shadow-md shadow-neutral-900  bg-neutral-800  text-center rounded-xl  ${props.class}`}>
-      <img src={image} class="text-xl w-full  font-bold" />
+    <div  class={`slide flex flex-col justify-around  p-2 pt-5 shadow-md shadow-neutral-900 z-0  bg-neutral-800  text-center rounded-xl  ${props.class}`}>
+
+      {isProjectFullscreen(title) && 
+        <Portal target={document.querySelector("body")}>
+          <div class="fixed inset-4 lg:inset-6 z-50" onClick={()=>toggleFullscreen(title)}>
+            <img src={image} class="w-full h-full object-cover" />
+          </div>
+        </Portal>
+      }
+      <img onClick={()=>toggleFullscreen(title)} src={image} class={ "w-full"} />
       <div class="text-2xl my-4 mb-2 font-bold">{title}</div>
       <div class=" text-muted">{description}</div>
       <div class="my-4 ">
-        <button class="text-green-500 hover:text-green-700">
+        <A class="text-green-500 hover:text-green-700" href={info}>
           See more
-        </button>
+        </A>
       </div>
     </div>
   )
@@ -61,9 +81,9 @@ export const Projects = () => {
         </div>
         <div class="p-4 px-5 md:p-20  overflow-clip">
 
-          <div class="flex gap-3 w-auto lg:hidden" use:slider>
+          <div class="flex gap-3 z-10 w-auto lg:hidden" use:slider>
             <For each={projects}>
-              {(project) => <Project {...project} class={`w-20`} />}
+              {(project) => <Project {...project} class={`z-0`} />}
             </For>
           </div>
 
---
+++ src/routes/frate.tsx
@@ -0,0 +1,8 @@
+
+export default function Frate() {
+  return (
+    <main class="  text-white">
+
+    </main>
+  );
+}
